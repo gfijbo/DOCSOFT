f3b597d71b5baa0946b558b556b036fe3d56ffee
Jamal BOUSFIHA <frup72389@GFI541588.annuaire.groupe.local>
2018-06-25 17:10:32 +0200

    maj listpage
    Signed-off-by: Jamal BOUSFIHA <frup72389@GFI541588.annuaire.groupe.local>

--- src/doc/PlatformBundle/Controller/ListPage.php
+++ src/doc/PlatformBundle/Controller/ListPage.php
@@ -63,4 +63,55 @@ class ListPage extends Controller
             'Content-Disposition' => 'inline; filename="' . $filename . '.pdf"'
         ));
     }
+    
+    /**
+     *
+     * @Route("/seechamp/{id}", name="seechamp")
+     */
+    public function seeChampAction($id)
+    {
+        $repository = $this->getdoctrine()
+        ->getManager()
+        ->getRepository('docPlatformBundle:DossierAgent');
+        
+        $doss = $repository->find($id);
+        $menu = "Documentation";
+        $urlPage = "";
+        
+        return $this->render('docPlatformBundle:DossierAgent:seeChamp.html.twig', array(
+            'doss' => $doss,
+            'menu' => $menu,
+            'urlPage' => $urlPage,
+            'listOnglets' => $_SESSION['listOnglets'],
+            'compteur' => count($_SESSION['listAlerts']),
+            'listAlerts' => $_SESSION['listAlerts']
+        ));
+    }
+    
+    /**
+     *
+     * @Route("/pdfchamp/{id}", name="seechamppdf")
+     */
+    public function pdfChampAction($id)
+    {
+        $filename = 'Etat_Civil';
+        $menu = "Dossier Agent";
+        $page = "Etat Civil";
+        $repository = $this->getdoctrine()
+        ->getManager()
+        ->getRepository('docPlatformBundle:DossierAgent');
+        
+        $doss = $repository->find($id);
+        $snappy = $this->get('knp_snappy.pdf');
+        $html = $this->renderView('docPlatformBundle:DossierAgent:pdfChamp.html.twig', array(
+            'doss' => $doss,
+            'page' => $page,
+            'menu' => $menu
+        ));
+        
+        return new Response($snappy->getOutputFromHtml($html), 200, array(
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'inline; filename="' . $filename . '.pdf"'
+        ));
+    }
 }
