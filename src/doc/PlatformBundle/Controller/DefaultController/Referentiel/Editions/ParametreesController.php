1f8d98280a1ac89354d2a9c5614fa9d110a69f84
Jamal BOUSFIHA <frup72389@GFI541588.annuaire.groupe.local>
2018-06-29 17:08:35 +0200

    maj  referentiel editions controller
    Signed-off-by: Jamal BOUSFIHA <frup72389@GFI541588.annuaire.groupe.local>

--- src/doc/PlatformBundle/Controller/DefaultController/Referentiel/Editions/ParametreesController.php
+++ src/doc/PlatformBundle/Controller/DefaultController/Referentiel/Editions/ParametreesController.php
@@ -1,1 +1,26 @@
 <?php
+namespace doc\PlatformBundle\Controller\DefaultController\Referentiel\Editions;
+
+use doc\PlatformBundle\Controller\ListPage;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ParametreesController extends ListPage
+{
+    /**
+     *
+     * @Route("/referentiel/editions/partitedi", name="seeEditionsPartitedi")
+     */
+    public function EditionsPartitediAction()
+    {
+        return $this->listChamp("Paramétrées / Titres éditions paramétrées","referentiel/editions/partitedi/pdf",'45', '204', "Référentiel");
+    }
+    
+    /**
+     *
+     * @Route("/referentiel/editions/partitedi/pdf", name="EditionsPartitedipdf")
+     */
+    public function EditionsPartitediPdfAction()
+    {
+        return $this->listChampPdf("Paramétrées / Titres éditions paramétrées",'45', '204', "Référentiel");
+    }
+}
