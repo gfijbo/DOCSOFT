0e2774840bc4de2dbcdcb90f5c671802fa2b36d5
Jamal BOUSFIHA <frup72389@GFI541588.annuaire.groupe.local>
2018-07-02 10:28:08 +0200

    maj table menuDoc.html.twig
    Signed-off-by: Jamal BOUSFIHA <frup72389@GFI541588.annuaire.groupe.local>

--- src/doc/PlatformBundle/Controller/DefaultController/Referentiel/Editions/DifferencesController.php
+++ src/doc/PlatformBundle/Controller/DefaultController/Referentiel/Editions/DifferencesController.php
@@ -1,1 +1,26 @@
 <?php
+namespace doc\PlatformBundle\Controller\DefaultController\Referentiel\Editions;
+
+use doc\PlatformBundle\Controller\ListPage;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DifferencesController extends ListPage
+{
+    /**
+     *
+     * @Route("/referentiel/editions/difjou", name="seeEditionsDifjou")
+     */
+    public function EditionsDifjouAction()
+    {
+        return $this->listChamp("Différences de paie / Codes différences paies","referentiel/editions/difjou/pdf",'46', '209', "Référentiel");
+    }
+    
+    /**
+     *
+     * @Route("/referentiel/editions/difjou/pdf", name="EditionsDifjoupdf")
+     */
+    public function EditionsDifjouPdfAction()
+    {
+        return $this->listChampPdf("Différences de paie / Codes différences paies",'46', '209', "Référentiel");
+    }
+}
