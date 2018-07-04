5bbd0e24d2b8fba09e48275e154a5189b935137e
Jamal BOUSFIHA <frup72389@GFI541588.annuaire.groupe.local>
2018-07-04 14:25:49 +0200

    maj
    Signed-off-by: Jamal BOUSFIHA <frup72389@GFI541588.annuaire.groupe.local>

--- src/doc/PlatformBundle/Controller/DefaultController/Referentiel/Prestation/BaremesController.php
+++ src/doc/PlatformBundle/Controller/DefaultController/Referentiel/Prestation/BaremesController.php
@@ -26,18 +26,69 @@ class BaremesController extends ListPage
     
     /**
      *
-     * @Route("/referentiel/prestation/bar/che", name="seePrestationBarChe")
+     * @Route("/referentiel/prestation/bar/bou", name="seePrestationBarBou")
      */
-    public function PrestationBarCheAction()
+    public function PrestationBarBouAction()
     {
-        return $this->listChamp("Barèmes / Chèques Vacances","referentiel/prestation/bar/che/pdf",'83', '348', "Référentiel");
+        return $this->listChamp("Barèmes / Bourses Etudes","referentiel/prestation/bar/bou/pdf",'83', '349', "Référentiel");
     }
     /**
      *
-     * @Route("/referentiel/prestation/bar/che/pdf", name="seePrestationBarChepdf")
+     * @Route("/referentiel/prestation/bar/bou/pdf", name="seePrestationBarBoupdf")
      */
-    public function PrestationBarChePdfAction()
+    public function PrestationBarBouPdfAction()
     {
-        return $this->listChampPdf("Barèmes / Chèques Vacances",'83', '348', "Référentiel");
+        return $this->listChampPdf("Barèmes / Bourses Etudes",'83', '349', "Référentiel");
+    }
+    
+    /**
+     *
+     * @Route("/referentiel/prestation/bar/sej", name="seePrestationBarSej")
+     */
+    public function PrestationBarSejAction()
+    {
+        return $this->listChamp("Barèmes / Séjour Enfants","referentiel/prestation/bar/sej/pdf",'83', '350', "Référentiel");
+    }
+    /**
+     *
+     * @Route("/referentiel/prestation/bar/sej/pdf", name="seePrestationBarSejpdf")
+     */
+    public function PrestationBarSejPdfAction()
+    {
+        return $this->listChampPdf("Barèmes / Séjour Enfants",'83', '350', "Référentiel");
+    }
+    
+    /**
+     *
+     * @Route("/referentiel/prestation/bar/typ", name="seePrestationBarTyp")
+     */
+    public function PrestationBarTypAction()
+    {
+        return $this->listChamp("Barèmes / Type Séjour Enfant","referentiel/prestation/bar/typ/pdf",'83', '351', "Référentiel");
+    }
+    /**
+     *
+     * @Route("/referentiel/prestation/bar/typ/pdf", name="seePrestationBarTyppdf")
+     */
+    public function PrestationBarTypPdfAction()
+    {
+        return $this->listChampPdf("Barèmes / Type Séjour Enfant",'83', '351', "Référentiel");
+    }
+    
+    /**
+     *
+     * @Route("/referentiel/prestation/bar/gar", name="seePrestationBarGar")
+     */
+    public function PrestationBarGarAction()
+    {
+        return $this->listChamp("Barèmes / Garde Enfant","referentiel/prestation/bar/gar/pdf",'83', '352', "Référentiel");
+    }
+    /**
+     *
+     * @Route("/referentiel/prestation/bar/gar/pdf", name="seePrestationBarGarpdf")
+     */
+    public function PrestationBarGarPdfAction()
+    {
+        return $this->listChampPdf("Barèmes / Garde Enfant",'83', '352', "Référentiel");
     }
 }
