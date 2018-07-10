2c6ee3dc2c9ed2a7f93e1e179836fe8bd3f5c07c
Jamal BOUSFIHA <frup72389@GFI541588.annuaire.groupe.local>
2018-07-10 17:36:07 +0200

    maj referentiel regime controller
    Signed-off-by: Jamal BOUSFIHA <frup72389@GFI541588.annuaire.groupe.local>

--- src/doc/PlatformBundle/Controller/DefaultController/Referentiel/Remuneration/RegimeController.php
+++ src/doc/PlatformBundle/Controller/DefaultController/Referentiel/Remuneration/RegimeController.php
@@ -23,4 +23,55 @@ class RegimeController extends ListPage
     {
         return $this->listChampPdf("Régime indemnitaire / Eléments de régime indemnitaire",'52', '266', "Référentiel");
     }
+    
+    /**
+     *
+     * @Route("/referentiel/remuneration/reg/pro", name="seeRemunerationRegPro")
+     */
+    public function RemunerationRegProAction()
+    {
+        return $this->listChamp("Régime indemnitaire / Profils d'attribution","referentiel/remuneration/reg/pro/pdf",'52', '267', "Référentiel");
+    }
+    /**
+     *
+     * @Route("/referentiel/remuneration/reg/pro/pdf", name="seeRemunerationRegPropdf")
+     */
+    public function RemunerationRegProPdfAction()
+    {
+        return $this->listChampPdf("Régime indemnitaire / Profils d'attribution",'52', '267', "Référentiel");
+    }
+    
+    /**
+     *
+     * @Route("/referentiel/remuneration/reg/rub", name="seeRemunerationRegRub")
+     */
+    public function RemunerationRegRubAction()
+    {
+        return $this->listChamp("Régime indemnitaire / Rubriques du régime indemnitaire","referentiel/remuneration/reg/rub/pdf",'52', '268', "Référentiel");
+    }
+    /**
+     *
+     * @Route("/referentiel/remuneration/reg/rub/pdf", name="seeRemunerationRegRubpdf")
+     */
+    public function RemunerationRegRubPdfAction()
+    {
+        return $this->listChampPdf("Régime indemnitaire / Rubriques du régime indemnitaire",'52', '268', "Référentiel");
+    }
+    
+    /**
+     *
+     * @Route("/referentiel/remuneration/reg/par", name="seeRemunerationRegPar")
+     */
+    public function RemunerationRegParAction()
+    {
+        return $this->listChamp("Régime indemnitaire / Paramètres de régime","referentiel/remuneration/reg/par/pdf",'52', '269', "Référentiel");
+    }
+    /**
+     *
+     * @Route("/referentiel/remuneration/reg/par/pdf", name="seeRemunerationRegParpdf")
+     */
+    public function RemunerationRegParPdfAction()
+    {
+        return $this->listChampPdf("Régime indemnitaire / Paramètres de régime",'52', '269', "Référentiel");
+    }
 }
