42b8586b7b76051cd9ab0ad8ce66d3572c9d3bba
sidihamed <36680076+sidihamed@users.noreply.github.com>
2018-07-16 17:29:37 +0200

    commit Dossier Traitement

--- src/doc/PlatformBundle/Controller/DefaultController/Traitement/Carriere/NotationsController.php
+++ src/doc/PlatformBundle/Controller/DefaultController/Traitement/Carriere/NotationsController.php
@@ -7,5 +7,36 @@ use doc\PlatformBundle\Controller\ListPage;
 
 class NotationsController extends ListPage
 {
-
+    /**
+ *
+ * @Route("/traitement/carriere/not/gen", name="seeCarriereNotGen")
+ */
+    public function CarriereNotGenAction()
+    {
+        return $this->listChamp("Notations / Génération notation","/traitement/carriere/not/gen/pdf",'127', '425', "Traitement");
+    }
+    /**
+     *
+     * @Route("/traitement/carriere/sai/not/gen/pdf", name="seeCarriereNotGenpdf")
+     */
+    public function CarriereNotGenPdfAction()
+    {
+        return $this->listChampPdf("Notations / Génération notation",'127', '425', "Traitement");
+    }
+    /**
+     *
+     * @Route("/traitement/carriere/not/edi", name="seeCarriereNotEdi")
+     */
+    public function CarriereNotEdiAction()
+    {
+        return $this->listChamp("Notations / Edition des fiches de notation","/traitement/carriere/not/edi/pdf",'127', '426', "Traitement");
+    }
+    /**
+     *
+     * @Route("/traitement/carriere/sai/not/edi/pdf", name="seeCarriereNotEdipdf")
+     */
+    public function CarriereNotEdiPdfAction()
+    {
+        return $this->listChampPdf("Notations / Edition des fiches de notation",'127', '426', "Traitement");
+    }
 }
